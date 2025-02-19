34e50efed118ccfe43c6da74da2b9e8f6cb7d527
Thasni Abbas <thasniabbas76@gmail.com>
2025-02-19 23:04:09 +0530

    Learnt adding js element to jsx using {}

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-
 //JSX React
-
 //JSX ==> React.creatElement is JS object = HTMLElement(render)
 const Title = () => ( 
     <h1 className="head">
@@ -11,12 +9,16 @@ const Title = () => (
     </h1>
 );// its written in jsx, the output are same for both core react and jsx
 
+
+const numelement = 50000 //we can add any javascript code in JSX using {} in component section
+
 //React Components
 //React functional components - normal javascript function
 //Syntax Option 1:
 const HeadingComponent = () => (
     <div id="component">
         <Title />
+        {numelement}
         <h1 className="heading"> React Functional component </h1>
     </div>
 );
