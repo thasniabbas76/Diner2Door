d3a0b346ab5fe5491024183166fbf5c98621b7b3
Thasni Abbas <thasniabbas76@gmail.com>
2025-03-08 21:16:35 +0530

    bg color

--- app.js
+++ app.js
@@ -43,8 +43,16 @@ const Header = () => {
 
 const RestaurantCard= () => {
     return(
-        <div className="res-card">
-            <h3>Diner2Door</h3>
+        <div className="res-card" style={{ backgroundColor:"#f0f0f0"}}>
+        <img
+        className="res-logo" 
+        alt="res-logo" 
+        src="https://img.freepik.com/free-photo/meat-burger-with-french-fries-side-view_141793-2771.jpg?semt=ais_hybrid"
+        />
+        <h4 style={{color:"white"}}>Paragon Restaurant</h4>
+        <h6>Biriyani, NorthIndian, Asian, Chinese</h6>
+        <h6>4.4 stars</h6>
+        <h6>38 minutes</h6>
         </div>
     );
 };
@@ -55,6 +63,17 @@ const Body = () =>{
         <div className="search">Search</div>
         <div className="res-container">
         <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+
         </div>
     </div>
     );
